40da47b97eda0f57f004c0fb8d5fa5d7f727fb71
brokenFdreams <brokenFdreams@protonmail.com>
2019-10-13 21:59:14 +0300

    DELETE Item

--- admin.php
+++ admin.php
@@ -1,6 +1,6 @@
 <?php
 include ("install.php");
-if($_POST['submititem'] == "OK") {
+if($_POST['submititem'] == "ADD") {
 	if ($_POST['item'] != "" && $_POST['price'] != "") {
 		$item = $_POST['item'];
 		$price = $_POST['price'];
@@ -10,11 +10,12 @@ if($_POST['submititem'] == "OK") {
 		} else
 			mysqli_query($link, "UPDATE item SET NAME='".$item."', Price='".$price."' WHERE id='".mysqli_num_rows($res)."';");
 	}
-} else if ($_POST['delitem'] == "DEL")
+} else if ($_POST['delitem'] == "DELETE")
 	if ($_POST['item'] != "") {
 		$item = $_POST['item'];
 		$res = mysqli_query($link, "SELECT Name FROM item where Name LIKE '".$item."';");
-		
+		if (mysqli_num_rows($res) != 0)
+			mysqli_query($link, "DELETE FROM item WHERE Name='".$item."';");
 	}
 ?>
 
@@ -26,8 +27,8 @@ require('header.php');
 	 <form action="admin.php" method="POST">
  		Item<input type="text" name="item" value=""><br />
 	 	Price<input type="text" name="price" value=""><br />
-		<input type="submit" name="submititem" value="OK"><br />
-		<input type="submit" name="delitem" valie="DEL"><br />
+		<input type="submit" name="submititem" value="ADD">
+		<input type="submit" name="delitem" value="DELETE"><br />
 	 </form>
 	 </div>
 	 <div class="cat">
